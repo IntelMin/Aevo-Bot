086910e77e1ee59663ef12279d9d11ddd44555c9
IntelMin <intelmin0915@gmail.com>
2024-01-26 19:41:44 +0100

    feat: update db on mainnet

--- database/db.py
+++ database/db.py
@@ -12,18 +12,18 @@ supabase: Client = create_client(supabase_url, supabase_key)
 def add_user(user_id: int, details: dict) -> None:
     _detail = json.dumps(details)
     detail = encrypt(_detail)
-    supabase.table('aevo_users').insert({"id": user_id, "details": detail}).execute()
+    supabase.table('aevo_users_mainnet').insert({"id": user_id, "details": detail}).execute()
 
 def get_user(user_id: int) -> Union[dict, None]:
-    data = supabase.table('aevo_users').select("*").match({"id": user_id}).execute().data
+    data = supabase.table('aevo_users_mainnet').select("*").match({"id": user_id}).execute().data
     if len(data) == 0:
         return None
     data = decrypt(data[0]["details"])
     return json.loads(data)
 
 def update_user(user_id, details):
-    supabase.table('aevo_users').update({"details": details}).eq("id", user_id).execute()
+    supabase.table('aevo_users_mainnet').update({"details": details}).eq("id", user_id).execute()
 
 def delete_user(user_id):
-    supabase.table('aevo_users').delete().eq("id", user_id).execute()
+    supabase.table('aevo_users_mainnet').delete().eq("id", user_id).execute()
 
